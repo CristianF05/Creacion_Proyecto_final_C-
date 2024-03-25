aff2d3d66b3a3040d6c1a7d0e2b5db98fe5616ca
CristianF05 <1482670@senati.pe>
2024-03-24 19:04:44 -0500

    Empesamos con el login

---
+++ Sistema_de_Registro_de_almacen/Login.cs
@@ -0,0 +1,10 @@
+namespace Sistema_de_Registro_de_almacen
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
