1ec94f6b9ad8534827ab1411a3048aa8efeddeba
CristianF05 <1482670@senati.pe>
2024-04-04 18:16:21 -0500

    error en conexion de la tabla Proevedor

--- Sistema_de_Alamcen/Sistema_de_Alamcen/Crear_Producto.Designer.cs
+++ Sistema_de_Alamcen/Sistema_de_Alamcen/Crear_Producto.Designer.cs
@@ -44,13 +44,15 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtFecha_vencimiento = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtImagen = new System.Windows.Forms.TextBox();
+            this.btnImagen = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -159,6 +161,7 @@
             this.btnGuardar.TabIndex = 37;
             this.btnGuardar.Text = "Crear";
             this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // label1
             // 
@@ -176,7 +179,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Green;
-            this.label2.Location = new System.Drawing.Point(494, 52);
+            this.label2.Location = new System.Drawing.Point(519, 52);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(131, 25);
             this.label2.TabIndex = 39;
@@ -198,7 +201,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Green;
-            this.label4.Location = new System.Drawing.Point(315, 52);
+            this.label4.Location = new System.Drawing.Point(363, 52);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(93, 25);
             this.label4.TabIndex = 41;
@@ -248,61 +251,80 @@
             this.label8.TabIndex = 45;
             this.label8.Text = "Fecha de vencimiento:";
             // 
-            // textBox1
+            // txtCategoria
             // 
-            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(368, 246);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(115, 25);
-            this.textBox1.TabIndex = 46;
+            this.txtCategoria.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategoria.Location = new System.Drawing.Point(368, 246);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(115, 25);
+            this.txtCategoria.TabIndex = 46;
             // 
-            // textBox2
+            // txtCantidad
             // 
-            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(375, 163);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(108, 25);
-            this.textBox2.TabIndex = 47;
+            this.txtCantidad.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(375, 163);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(108, 25);
+            this.txtCantidad.TabIndex = 47;
             // 
-            // textBox3
+            // txtNombre
             // 
-            this.textBox3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(360, 80);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(123, 25);
-            this.textBox3.TabIndex = 48;
+            this.txtNombre.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(390, 78);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(123, 25);
+            this.txtNombre.TabIndex = 48;
             // 
-            // textBox4
+            // txtPrecio
             // 
-            this.textBox4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(550, 163);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 25);
-            this.textBox4.TabIndex = 49;
+            this.txtPrecio.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(550, 163);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 25);
+            this.txtPrecio.TabIndex = 49;
             // 
-            // textBox5
+            // txtFecha_vencimiento
             // 
-            this.textBox5.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(550, 246);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 25);
-            this.textBox5.TabIndex = 50;
+            this.txtFecha_vencimiento.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFecha_vencimiento.Location = new System.Drawing.Point(550, 246);
+            this.txtFecha_vencimiento.Name = "txtFecha_vencimiento";
+            this.txtFecha_vencimiento.Size = new System.Drawing.Size(100, 25);
+            this.txtFecha_vencimiento.TabIndex = 50;
             // 
-            // textBox6
+            // txtDescripcion
             // 
-            this.textBox6.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(550, 80);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(160, 25);
-            this.textBox6.TabIndex = 51;
+            this.txtDescripcion.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(575, 83);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(160, 25);
+            this.txtDescripcion.TabIndex = 51;
             // 
-            // textBox7
+            // txtCodigo
             // 
-            this.textBox7.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(179, 239);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 25);
-            this.textBox7.TabIndex = 52;
+            this.txtCodigo.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(179, 239);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 25);
+            this.txtCodigo.TabIndex = 52;
+            // 
+            // txtImagen
+            // 
+            this.txtImagen.Location = new System.Drawing.Point(167, 85);
+            this.txtImagen.Name = "txtImagen";
+            this.txtImagen.Size = new System.Drawing.Size(100, 20);
+            this.txtImagen.TabIndex = 53;
+            // 
+            // btnImagen
+            // 
+            this.btnImagen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnImagen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImagen.Location = new System.Drawing.Point(273, 84);
+            this.btnImagen.Name = "btnImagen";
+            this.btnImagen.Size = new System.Drawing.Size(56, 23);
+            this.btnImagen.TabIndex = 54;
+            this.btnImagen.Text = "Guardar";
+            this.btnImagen.UseVisualStyleBackColor = false;
+            this.btnImagen.Click += new System.EventHandler(this.btnImagen_Click);
             // 
             // Crear_Producto
             // 
@@ -310,13 +332,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleTurquoise;
             this.ClientSize = new System.Drawing.Size(747, 376);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnImagen);
+            this.Controls.Add(this.txtImagen);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtFecha_vencimiento);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtCategoria);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -335,6 +359,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Crear_Producto";
             this.Text = "Crear_Producto";
+            this.Load += new System.EventHandler(this.Crear_Producto_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -359,12 +384,14 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtFecha_vencimiento;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtImagen;
+        private System.Windows.Forms.Button btnImagen;
     }
 }
--- Sistema_de_Alamcen/Sistema_de_Alamcen/Crear_Producto.cs
+++ Sistema_de_Alamcen/Sistema_de_Alamcen/Crear_Producto.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Sistema_de_Alamcen
 {
     public partial class Crear_Producto : Form
     {
+        private string connectionString = "Server=DESKTOP-HONFTB2;Database=Almacen;Integrated Security=true;"; // Reemplaza con tu cadena de conexión
+
         public Crear_Producto()
         {
             InitializeComponent();
@@ -101,6 +98,109 @@ namespace Sistema_de_Alamcen
 
             // Mostrar la página Pagina_Inicial
             salida.Show();
+        }
+        private byte[] ObtenerBytesDeImagen(string rutaArchivo)
+        {
+            try
+            {
+                // Verificar si la ruta de archivo es válida
+                if (File.Exists(rutaArchivo))
+                {
+                    // Leer los bytes del archivo de imagen
+                    return File.ReadAllBytes(rutaArchivo);
+                }
+                else
+                {
+                    MessageBox.Show("La imagen seleccionada no existe en la ruta especificada.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener bytes de la imagen: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Obtener los valores de los campos de texto y la imagen
+            string nombre = txtNombre.Text;
+            string descripcion = txtDescripcion.Text;
+            string cantidadStock = txtCantidad.Text.Trim(); // Renombrado para que coincida con el nombre de la columna en la base de datos
+            decimal precio;
+            if (!decimal.TryParse(txtPrecio.Text, out precio))
+            {
+                MessageBox.Show("Por favor, ingrese un valor de precio válido.", "Error de Precio", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string sku = txtCodigo.Text.Trim(); // Renombrado para que coincida con el nombre de la columna en la base de datos
+            string categoria = txtCategoria.Text;
+            DateTime fechaVencimiento;
+            if (!DateTime.TryParse(txtFecha_vencimiento.Text, out fechaVencimiento))
+            {
+                MessageBox.Show("Por favor, ingrese una fecha de vencimiento válida.", "Error de Fecha de Vencimiento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            byte[] imagen = ObtenerBytesDeImagen(txtImagen.Text); // Obtener los bytes de la imagen desde el TextBox
+
+            // Establecer la consulta SQL para la inserción de datos
+            string query = @"INSERT INTO Producto (Nombre, Descripcion, Cantidad_Stock, Precio, SKU, Categoria, Fecha_vencimiento, Imagen)
+             VALUES (@Nombre, @Descripcion, @Cantidad_Stock, @Precio, @SKU, @Categoria, @Fecha_vencimiento, @Imagen)";
+
+            // Crear y configurar el comando SQL
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                // Asignar los parámetros
+                command.Parameters.AddWithValue("@Nombre", nombre);
+                command.Parameters.AddWithValue("@Descripcion", descripcion);
+                command.Parameters.AddWithValue("@Cantidad_Stock", cantidadStock);
+                command.Parameters.AddWithValue("@Precio", precio);
+                command.Parameters.AddWithValue("@SKU", sku);
+                command.Parameters.AddWithValue("@Categoria", categoria);
+                command.Parameters.AddWithValue("@Fecha_vencimiento", fechaVencimiento);
+                command.Parameters.AddWithValue("@Imagen", imagen);
+
+                try
+                {
+                    // Abrir la conexión y ejecutar la consulta
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Producto guardado exitosamente.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar el producto: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+        private void Crear_Producto_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImagen_Click(object sender, EventArgs e)
+        {
+            // Crear un nuevo cuadro de diálogo para seleccionar archivo
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Establecer las propiedades del cuadro de diálogo
+            openFileDialog.Title = "Seleccionar Imagen";
+            openFileDialog.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog.Multiselect = false;
+
+            // Mostrar el cuadro de diálogo
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Obtener la ruta del archivo seleccionado y mostrarla en el TextBox
+                string rutaImagen = openFileDialog.FileName;
+                txtImagen.Text = rutaImagen;
+            }
         }
     }
 }
